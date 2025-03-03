d89d6f4ebff9623b05ee9a8ae2e6f14468a58879
kishoreabc <kishorehp134@gmail.com>
2025-03-03 23:28:20 +0530

    Added Google OAuth Login functionalities

--- CareerGuidance--/src/App.jsx
+++ CareerGuidance--/src/App.jsx
@@ -1,15 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import Dashboard from "./pages/dashboard";
 
-import './App.css'
-import LoginPage from './LoginPage'
-
-
-function App() {
+const App = () => {
   return (
-    <>
-    
-      <LoginPage></LoginPage>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
